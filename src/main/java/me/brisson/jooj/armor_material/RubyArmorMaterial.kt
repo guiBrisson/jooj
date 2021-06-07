bf82b54cfcae4537f8807d685b83797806ecb24e
guiBrisson <gbrisson98@gmail.com>
2021-06-06 23:30:03 -0300

    Adding Ruby Armor

---
+++ src/main/java/me/brisson/jooj/armor_material/RubyArmorMaterial.kt
@@ -0,0 +1,50 @@
+package me.brisson.jooj.armor_material
+
+import me.brisson.jooj.registry.ModItems
+import net.minecraft.entity.EquipmentSlot
+import net.minecraft.item.ArmorMaterial
+import net.minecraft.recipe.Ingredient
+import net.minecraft.sound.SoundEvent
+import net.minecraft.sound.SoundEvents
+
+
+class RubyArmorMaterial: ArmorMaterial {
+
+    companion object {
+        // item Order(helmet, chestplate, leggings, boots)
+        private val PROTECTION_VALUES = intArrayOf(4, 7, 9, 4)
+        private val BASE_DURABILITY = intArrayOf(13, 15, 16, 11)
+    }
+
+    override fun getDurability(slot: EquipmentSlot): Int {
+        return PROTECTION_VALUES[slot.entitySlotId] * 40
+    }
+
+    override fun getProtectionAmount(slot: EquipmentSlot): Int {
+        return PROTECTION_VALUES[slot.entitySlotId]
+    }
+
+    override fun getEnchantability(): Int {
+        return 15
+    }
+
+    override fun getEquipSound(): SoundEvent {
+        return SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND
+    }
+
+    override fun getRepairIngredient(): Ingredient {
+        return Ingredient.ofItems(ModItems.ruby)
+    }
+
+    override fun getName(): String {
+        return "ruby"
+    }
+
+    override fun getToughness(): Float {
+        return 3.0f
+    }
+
+    override fun getKnockbackResistance(): Float {
+        return 0.1F
+    }
+}
--- src/main/java/me/brisson/jooj/registry/ModItems.kt
+++ src/main/java/me/brisson/jooj/registry/ModItems.kt
@@ -1,9 +1,9 @@
 package me.brisson.jooj.registry
 
 import me.brisson.jooj.Jooj
-import net.minecraft.item.BlockItem
-import net.minecraft.item.Item
-import net.minecraft.item.ItemGroup
+import me.brisson.jooj.armor_material.RubyArmorMaterial
+import net.minecraft.entity.EquipmentSlot
+import net.minecraft.item.*
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
 
@@ -11,20 +11,36 @@ class ModItems {
 
 
     fun registerItems() {
+        // Items
         Registry.register(Registry.ITEM, Identifier(Jooj.MOD_ID, "ruby"), ruby)
+        // Blocks
         Registry.register(Registry.ITEM, Identifier(Jooj.MOD_ID, "ruby_block"), rubyBlock)
         Registry.register(Registry.ITEM, Identifier(Jooj.MOD_ID, "ruby_ore"), rubyOre)
+        // Armor
+        Registry.register(Registry.ITEM, Identifier(Jooj.MOD_ID, "ruby_helmet"), rubyHelmet)
+        Registry.register(Registry.ITEM, Identifier(Jooj.MOD_ID, "ruby_chestplate"), rubyChestplate)
+        Registry.register(Registry.ITEM, Identifier(Jooj.MOD_ID, "ruby_leggings"), rubyLeggings)
+        Registry.register(Registry.ITEM, Identifier(Jooj.MOD_ID, "ruby_boots"), rubyBoots)
+
     }
 
     companion object {
         private val itemGroup: ItemGroup = Jooj.itemGroup
+        private val rubyArmorMaterial = RubyArmorMaterial()
 
         // Items
         val ruby = Item(Item.Settings().group(itemGroup))
 
+        //  Ruby Armor
+        val rubyHelmet = ArmorItem(rubyArmorMaterial, EquipmentSlot.HEAD, Item.Settings().group(itemGroup))
+        val rubyChestplate = ArmorItem(rubyArmorMaterial, EquipmentSlot.CHEST, Item.Settings().group(itemGroup))
+        val rubyLeggings = ArmorItem(rubyArmorMaterial, EquipmentSlot.LEGS, Item.Settings().group(itemGroup))
+        val rubyBoots = ArmorItem(rubyArmorMaterial, EquipmentSlot.FEET, Item.Settings().group(itemGroup))
+
         //Block Items
         private val rubyBlock =  BlockItem(ModBlocks.rubyBlock, Item.Settings().group(itemGroup))
         private val rubyOre = BlockItem(ModBlocks.rubyOre, Item.Settings().group(itemGroup))
+
     }
 
 }
